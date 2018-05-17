53113c751bae893735aca29d195d648e1725accb
Darin Dimitrov <darin.dimitrov@gmail.com>
2018-05-17 17:54:43 +0300

    Add experimental support for ES6 modules

--- test-app/runtime/src/main/java/com/tns/AppConfig.java
+++ test-app/runtime/src/main/java/com/tns/AppConfig.java
@@ -19,7 +19,8 @@ class AppConfig {
         MarkingMode("markingMode", com.tns.MarkingMode.full),
         HandleTimeZoneChanges("handleTimeZoneChanges", false),
         MaxLogcatObjectSize("maxLogcatObjectSize", 1024),
-        ForceLog("forceLog", false);
+        ForceLog("forceLog", false),
+        ExperimentalModules("experimentalModules", false);
 
         private final String name;
         private final Object defaultValue;
@@ -112,6 +113,9 @@ class AppConfig {
                     if (androidObject.has(KnownKeys.ForceLog.getName())) {
                         values[KnownKeys.ForceLog.ordinal()] = androidObject.getBoolean(KnownKeys.ForceLog.getName());
                     }
+                    if (androidObject.has(KnownKeys.ExperimentalModules.getName())) {
+                        values[KnownKeys.ExperimentalModules.ordinal()] = androidObject.getBoolean(KnownKeys.ExperimentalModules.getName());
+                    }
                 }
             }
         } catch (Exception e) {
@@ -161,5 +165,9 @@ class AppConfig {
 
     public boolean getForceLog() {
         return (boolean)values[KnownKeys.ForceLog.ordinal()];
+    }
+
+    public boolean isExperimentalModules() {
+        return (boolean)values[KnownKeys.ExperimentalModules.ordinal()];
     }
 }
--- test-app/runtime/src/main/java/com/tns/Runtime.java
+++ test-app/runtime/src/main/java/com/tns/Runtime.java
@@ -32,7 +32,7 @@ import android.util.SparseArray;
 import com.tns.bindings.ProxyGenerator;
 
 public class Runtime {
-    private native void initNativeScript(int runtimeId, String filesPath, String nativeLibDir, boolean verboseLoggingEnabled, boolean isDebuggable, String packageName, Object[] v8Options, String callingDir, int maxLogcatObjectSize, boolean forceLog);
+    private native void initNativeScript(int runtimeId, String filesPath, String nativeLibDir, boolean verboseLoggingEnabled, boolean isDebuggable, String packageName, Object[] v8Options, String callingDir, int maxLogcatObjectSize, boolean forceLog, boolean experimentalModules);
 
     private native void runModule(int runtimeId, String filePath) throws NativeScriptException;
 
@@ -491,9 +491,10 @@ public class Runtime {
             }
 
             boolean forceConsoleLog = appConfig.getForceLog() || "timeline".equalsIgnoreCase(appConfig.getProfilingMode());
+            boolean experimentalModules = appConfig.isExperimentalModules();
 
             initNativeScript(getRuntimeId(), Module.getApplicationFilesPath(), nativeLibDir, logger.isEnabled(), isDebuggable, appName, appConfig.getAsArray(),
-                    callingJsDir, appConfig.getMaxLogcatObjectSize(), forceConsoleLog);
+                             callingJsDir, appConfig.getMaxLogcatObjectSize(), forceConsoleLog, experimentalModules);
 
             //clearStartupData(getRuntimeId()); // It's safe to delete the data after the V8 debugger is initialized
 
